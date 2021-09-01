5ab1573ddf9ff137e357fff6860e6ab35e1f14e9
haruto17 <proaca157@gmail.com>
2021-09-01 22:47:06 +0900

    add GUIの作成

--- MainProject/KosenProcon32/GUI.cpp
+++ MainProject/KosenProcon32/GUI.cpp
@@ -1,1 +1,23 @@
 #include"GUI.hpp"
+
+void Procon32::GUI::draw() 
+{
+	bigFont(U"Hello World!").draw(100, 100, Palette::Red);
+}
+
+void Procon32::GUI::dataUpdate()
+{
+
+}
+
+Procon32::GUI::GUI()
+{
+	bigFont = Font(30, Typeface::Bold);
+	smallFont = Font(24, Typeface::Bold);
+
+	test = Font((int32)(50, 0.46));
+}
+
+Procon32::GUI::~GUI()
+{
+}
--- MainProject/KosenProcon32/Main.cpp
+++ MainProject/KosenProcon32/Main.cpp
@@ -1,15 +1,22 @@
 ﻿#include"KosenProcon32.hpp"
+#include"GUI.hpp"
 
 void Main()
 {
-	
+	Window::SetTitle(U"KosenProcon32");
+	Scene::SetScaleMode(ScaleMode::AspectFit);
+	Window::SetStyle(WindowStyle::Sizable);
+	Scene::SetBackground(ColorF(0.8, 0.9, 1.0));
 
+	Procon32::GUI gui;
 
-
+	gui.dataUpdate();
 
 	while (System::Update())
 	{
+		gui.draw();
 
+		Circle(Cursor::Pos(), 30).draw(ColorF(1, 0, 0, 0.5));
 	}
 
 
