dd4a09cd54795cf1e2fec5ef4f7d87332f675711
haruto17 <proaca157@gmail.com>
2021-10-01 15:56:09 +0900

    aaaa

---
+++ MainProject/Computation/KosenProcon32.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#include<iostream>
+#include<string>
+#include<string.h>
+#include<algorithm>
+#include<fstream>
+#include<opencv2/opencv.hpp>
+#include<curl.h>
+
+
+namespace Procon32
+{
+	
+}
